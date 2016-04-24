846e2f621f4658902b197ec95bcd51dc721973c5
onnbrian <bon@princeton.edu>
2016-04-24 14:55:56 -0400

    scraping file

--- live_scraping/live_data_scrape.py
+++ live_scraping/live_data_scrape.py
@@ -9,7 +9,6 @@ def get_train_vals(trainNum):
   page = urllib2.urlopen(base_url + trainNum)
   soup = BeautifulSoup(page.read(), "html5lib")
   string = soup.find_all("td")
-  
   totalString = "[\n"
 
   for s in range(len(string)):
@@ -36,51 +35,11 @@ def get_train_vals(trainNum):
         totalString += ","
         totalString += "\n"
 
-  print totalString
-  print totalString[-2]
   if(totalString[:-2]):
     totalString = totalString[:-2]
     totalString += "\n]"
     #  print totalString
     jsonobj = json.loads(totalString)
     return jsonobj
-  else:
-    return "hi"
 
-print get_train_vals(str(7829))
-
-''' 
-#string = get_train_vals(sys.argv[1])
-totalString = "[\n"
-
-for s in range(len(string)):
-    if(string[s].find("p")):
-        text = string[s].find("p").get_text()
-        text2 = " ".join(text.split())
-#        print text2
-        splitText = text2.split()
-        totalString += "    {\n        \"location\": \""
-        if " at " in text2:
-          for i in range(len(splitText) - 2):
-            totalString += splitText[i]
-            if(i != len(splitText)-3):
-              totalString += " "
-        else:
-          for i in range(len(splitText) - 1):
-            totalString += splitText[i]
-            if(i != len(splitText)-2):
-              totalString += " "
-
-        totalString += "\",\n        \"time\":"
-        totalString += " \"" + splitText[len(splitText)-1] + "\""
-        totalString += "\n    }"
-        totalString += ","
-        totalString += "\n"
-
-if(totalString[:-2]):
-  totalString = totalString[:-2]
-  totalString += "\n]"
-#  print totalString
-  jsonobj = json.loads(totalString)
-  print jsonobj
-'''
+  
